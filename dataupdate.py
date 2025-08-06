4450beb16d541415db0eeb0332e02ad6b60d2b25
emilkm <emilkm@gmail.com>
2025-08-06 19:02:46 +1200

    cur

--- dataupdate.py
+++ dataupdate.py
@@ -3,17 +3,24 @@ import pandas as pd
 import codecs as cd
 from git import Repo
 
-url = 'https://docs.google.com/spreadsheets/d/1gBftBUJqezG2QjUij7YBoZYQswpGYPYFmX9V3bfZues/gviz/tq?tqx=out:csv&sheet=Glossary'
+url = 'https://docs.google.com/spreadsheets/d/1jewXALlsOojcegEiGAWYvncWaUy1g8oRE5Oup68bfP0/gviz/tq?tqx=out:csv&sheet=Glossary'
 r = rq.get(url, allow_redirects=True)
 open('data.csv', 'wb').write(r.content)
 
-with open('data.csv', 'r', encoding='utf8') as fin:
-    data = fin.read().splitlines(True)
-with open('data.csv', 'w', encoding='utf8') as fout:
-    fout.write('devn,iast,part,gend,engl,pada\n')
-    fout.writelines(data[2:])
 
-df = pd.read_csv(cd.open('data.csv', 'r', 'utf-8'))
+
+new_columns = [
+    'artist', 'song', 'userdifficulty', 'usernotes', 'mmmdifficulty',
+    'mmmnotes', 'style', 'learn', 'tuning', 'mmmtutorial', 'timesignature'
+]
+df = pd.read_csv('data.csv', header=0)
+print(f"Detected columns: {list(df.columns)}")
+print(f"Number of columns: {len(df.columns)}")
+
+df = df.iloc[:, :11]  # Select only the first 11 columns
+df.columns = new_columns
+
+
 print(df.to_json(r'data.json', force_ascii=False, orient='records', lines=True))
 
 with open('data.json', 'r', encoding='utf8') as fin:
@@ -28,10 +35,10 @@ with open('data.json', 'w', encoding='utf8') as fout:
             fout.write(line.replace('\n', ',').strip() + '\n')
     fout.write(']\n')
 
-rp = Repo('.')
-rp.index.add(['data.json'])
-rp.index.commit('update data')
-origin = rp.remote('origin')
-origin.push()
+#rp = Repo('.')
+#rp.index.add(['data.json'])
+#rp.index.commit('update data')
+#origin = rp.remote('origin')
+#origin.push()
 
 
